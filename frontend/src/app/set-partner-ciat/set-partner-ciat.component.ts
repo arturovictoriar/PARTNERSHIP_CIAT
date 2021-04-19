7cbac560e35fe2b3eabd55fe0eaa13418b6a52d3
arturovictoriar <1231@holbertonschool.com>
2021-04-18 20:34:42 -0500

    Same headquarter,name, acronyn, city or country validation

--- frontend/src/app/set-partner-ciat/set-partner-ciat.component.ts
+++ frontend/src/app/set-partner-ciat/set-partner-ciat.component.ts
@@ -13,6 +13,7 @@ import isoCountries from '../../assets/en.json';
 export class SetPartnerCiatComponent implements OnInit {
   public registerForm: any;
   public invalid_form: any;
+  public invalid_name: any;
   public countries: any;
   public error: any;
   public placeholderText = {
@@ -65,21 +66,53 @@ export class SetPartnerCiatComponent implements OnInit {
   }
 
   ngAfterContentChecked() {
-    this.cdref.detectChanges();  
+    this.cdref.detectChanges();
   }
 
   get f() {
     return this.registerForm.controls;
   }
 
+  validUniqueCityCountry (formValues, partner) {
+    if (partner.city === formValues.city || partner.country === formValues.country) {
+      return(false);
+    }
+    return(true);
+  }
+
+  async validUniqueName(formValues) {
+    let url = "http://localhost:5000/partners";
+    let allPartners = null;
+    try {
+      allPartners = await this.http.get(url).pipe(map((res) => res)).toPromise();
+    } catch (error) {
+      return(false);
+    }
+    for (let partner of allPartners) {
+      if (partner.name === formValues.name || partner.acronym === formValues.acronym) {
+        if (this.isBrand && this.validUniqueCityCountry(formValues, partner)) {
+          console.log("sali por aca")
+          return(true);
+        }
+        return(false);
+      }
+    }
+    console.log("sali")
+    return(true);
+  }
+
   async onSubmit() {
     let formValues = this.registerForm.value;
+    if (!(await this.validUniqueName(formValues))) {
+      this.invalid_name = true;
+      return;
+    }
     if (this.registerForm.invalid || (formValues.headquarter === null && this.isBrand)) {
       this.invalid_form = true;
       return;
     }
     this.invalid_form = false;
-    console.log(formValues);
+    this.invalid_name = false;
     let host = "http://localhost:5000"
     let url = host + "/partners";
     let data = {
@@ -89,10 +122,9 @@ export class SetPartnerCiatComponent implements OnInit {
       "type": formValues.type,
       "city": formValues.city,
       "partner_url": formValues.partnerUrl ? formValues.partnerUrl : "",
-      "headquarter": this.isBrand ? "no" : "yes",
+      "headquarter": this.isBrand ? "NO" : "YES",
     };
     let result = await this.http.post(url, data).pipe(map((res) => res)).toPromise();
-    console.log(result);
     this.router.navigate(['partners']);
   }
 
